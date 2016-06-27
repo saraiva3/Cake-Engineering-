a888e317ff60cd30519ce4da7dfd524ccf91ef0f
hcdias <hugo_root@yahoo.com.br>
2016-06-27 01:51:58 -0300

    Adicionar nova configuracao de vm

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install python-software-properties -y
+sudo add-apt-repository ppa:ondrej/php5-5.6
+sudo apt-get update 
+sudo apt-get install -y apache2-bin php5-common php5-cli libapache2-mod-php5 php-pear
