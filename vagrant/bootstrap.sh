fc8e0de96b73e3e0fe12495def9feca3d74bc96c
hcdias <hugo_root@yahoo.com.br>
2016-07-04 00:34:58 -0300

    Adicionar novas dependecias de projeto ao provisionamento

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -3,4 +3,4 @@ sudo apt-get update
 sudo apt-get install python-software-properties -y
 sudo add-apt-repository ppa:ondrej/php5-5.6
 sudo apt-get update 
-sudo apt-get install -y apache2-bin php5-common php5-cli libapache2-mod-php5 php-pear
+sudo apt-get install -y apache2-bin php5-common php5-cli libapache2-mod-php5 php-pear libmysqlclient18 mysql-common php5-mysql php5-intl
